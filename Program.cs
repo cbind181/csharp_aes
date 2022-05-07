447c4d343aa78c3c87c148f6ae8ea0703df2f20f
cbind181 <33586994+cbind181@users.noreply.github.com>
2022-05-07 01:18:13 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Aes_Example
+{
+    class AesExample
+    {
+        public static void Main()
+        {
+
+
+            string original = string.Join("", System.IO.File.ReadAllLines(@"C:\Users\Calen.Olsen\source\repos\AESCBC test\AESCBC test\small.txt"));
+
+            // Create a new instance of the Aes
+            // class.  This generates a new key and initialization
+            // vector (IV).
+            using (Aes myAes = Aes.Create())
+            {
+
+                long bestEncrypt = 999999;
+                long bestDecrypt = 999999;
+
+                for(int i = 0; i < 20; i++)
+                {
+                    var watch = new System.Diagnostics.Stopwatch();
+
+                    watch.Start();
+                    byte[] encrypted = EncryptStringToBytes_Aes(original, myAes.Key, myAes.IV);
+                    watch.Stop();
+
+                    if (watch.ElapsedMilliseconds < bestEncrypt)
+                    {
+                        bestEncrypt = watch.ElapsedMilliseconds;
+                    }
+
+                    Console.WriteLine($"encrypt Time: {watch.ElapsedMilliseconds} ms");
+
+
+                    watch = new System.Diagnostics.Stopwatch();
+
+                    watch.Start();
+                    string roundtrip = DecryptStringFromBytes_Aes(encrypted, myAes.Key, myAes.IV);
+                    watch.Stop();
+
+                    Console.WriteLine($"encrypt Time: {watch.ElapsedMilliseconds} ms");
+
+                    if (watch.ElapsedMilliseconds < bestDecrypt)
+                    {
+                        bestDecrypt = watch.ElapsedMilliseconds;
+                    }
+
+                    // Decrypt the bytes to a string.
+
+                }
+                Console.WriteLine("best Encrypt TIme {0}", bestEncrypt);
+                Console.WriteLine("best Decrypt TIme {0}", bestDecrypt);
+
+
+            }
+        }
+        static byte[] EncryptStringToBytes_Aes(string plainText, byte[] Key, byte[] IV)
+        {
+            // Check arguments.
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+            byte[] encrypted;
+
+            // Create an Aes object
+            // with the specified key and IV.
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+
+                // Create an encryptor to perform the stream transform.
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for encryption.
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            //Write all data to the stream.
+                            swEncrypt.Write(plainText);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+
+            // Return the encrypted bytes from the memory stream.
+            return encrypted;
+        }
+
+        static string DecryptStringFromBytes_Aes(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            // Check arguments.
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+
+            // Declare the string used to hold
+            // the decrypted text.
+            string plaintext = null;
+
+            // Create an Aes object
+            // with the specified key and IV.
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+
+                // Create a decryptor to perform the stream transform.
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for decryption.
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+
+                            // Read the decrypted bytes from the decrypting stream
+                            // and place them in a string.
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+
+            return plaintext;
+        }
+    }
+}
